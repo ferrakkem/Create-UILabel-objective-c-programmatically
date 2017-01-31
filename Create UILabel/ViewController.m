ac5fb84206739bdc54c82fa0faa54c4c38cfa3fa
ferrakkem <ferrakkem@gmail.com>
2017-01-31 15:31:56 +0600

    Create-UILabel-objective-c-programmatically

--- Create UILabel/ViewController.m
+++ Create UILabel/ViewController.m
@@ -14,13 +14,36 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"Hi Ferrakkem");
+    
+#pragma mark:: call_UILabel
+    [self createUILabel];
 }
 
+#pragma mark:::Create UIlabel
+- (void)createUILabel
+{
+    UILabel *myLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 50, 300-20, 50)];
+    myLabel.backgroundColor = [UIColor clearColor];
+    myLabel.textAlignment = NSTextAlignmentCenter;
+    myLabel.textColor = [UIColor blueColor];
+    myLabel.numberOfLines = 0;
+    myLabel.lineBreakMode = UILineBreakModeWordWrap;
+    myLabel.text = @"Submit";
+#pragma mark :: set_UILabel_borderWidth_Border_color_cornerRadious
+    myLabel.layer.borderWidth = 1.0;
+    myLabel.layer.borderColor = [UIColor darkGrayColor].CGColor;
+    myLabel.layer.cornerRadius = 5.0;
+    
+   [self.view addSubview:myLabel];
+    
+}
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
